6d5e1bced5bbce42252eefc6281adf7b396bc7dc
Ahmed-Elshehawy <a.elshehawy64@gmail.com>
2022-03-14 21:47:45 +0200

    create variable ml_path for ml_model_path

--- app/app_docker.py
+++ app/app_docker.py
@@ -34,8 +34,9 @@ drop_prob = 0.5
 
 model_lstm_no_stem = ah.load_lstm_model(weighths_path, vocab_size, output_size, embedding_dim,
                                         hidden_dim, n_layers, seq_length, drop_prob, use_cuda)
-                                        
-#model_ml_no_stem = ah.load_ml_model()                                        
+
+ml_path = root+'no_stem/models/pipe_rf_20.obj'                                        
+model_ml_no_stem = ah.load_ml_model(ml_path)                                        
 
 
 app = dash.Dash(name='Arabic_Dialect_Classification')
